d13a4b3f883d995e0532b0c54b549816b7fc9b9c
mistrc <chiragwork@poppyfields.biz>
2025-12-07 10:29:16 +0000

    added 2025 day03

---
+++ 2025/day03/bin/day03.dart
@@ -0,0 +1,11 @@
+import 'package:day03/day03.dart' as day03;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day03.calculate()}!');
+
+  print('sample test results ${day03.part1(true)}');
+  print('actual test results ${day03.part1(false)}');
+
+  print('sample test results ${day03.part2(true)}');
+  print('actual test results ${day03.part2(false)}');
+}
---
+++ 2025/day03/test/day03_test.dart
@@ -0,0 +1,8 @@
+import 'package:day03/day03.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
